e67ca53a41bfd4d7fbfb5ee47ec6bdd5436bed63
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2020-01-23 13:20:33 +0100

    Upload Images Funcionality Added

--- eShop/eShop.WebUI/Controllers/ProductManagerController.cs
+++ eShop/eShop.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -41,7 +42,7 @@ namespace eShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product,HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -49,6 +50,12 @@ namespace eShop.WebUI.Controllers
             }
             else
             {
+
+                if (file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//")+product.Image);
+                }
                 context.Insert(product);
                 context.Commit();
 
@@ -74,7 +81,7 @@ namespace eShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product, string Id)
+        public ActionResult Edit(Product product, string Id,HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
 
@@ -90,9 +97,15 @@ namespace eShop.WebUI.Controllers
                 }
                 else
                 {
+                    if (file != null)
+                    {
+                        productToEdit.Image = productToEdit.Id + Path.GetExtension(file.FileName);
+                        file.SaveAs(Server.MapPath("//Content//ProductImages//") + productToEdit.Image);
+                    }
+
                     productToEdit.Description = product.Description;
                     productToEdit.Category = product.Category;
-                    productToEdit.Image = product.Image;
+                 
                     productToEdit.Price = product.Price;
                     productToEdit.Name = product.Name;
 
