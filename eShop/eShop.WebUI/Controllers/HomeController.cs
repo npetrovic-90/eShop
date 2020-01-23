468c2315ea31d7cb57b3ab85ad40532b94f9481a
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2020-01-23 13:55:05 +0100

    Listing all products and details about them

--- eShop/eShop.WebUI/Controllers/HomeController.cs
+++ eShop/eShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using eShop.Core.Contracts;
+using eShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +8,35 @@ using System.Web.Mvc;
 
 namespace eShop.WebUI.Controllers
 {
+	
 	public class HomeController : Controller
 	{
+		IRepository<Product> context;
+		IRepository<ProductCategory> productCategories;
+
+		public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategories)
+		{
+			context = productContext;
+			this.productCategories = productCategories;
+
+
+		}
 		public ActionResult Index()
 		{
-			return View();
+			List<Product> products = context.Collection().ToList();
+			return View(products);
+		}
+
+		public ActionResult Details(string Id)
+		{
+			Product product = context.Find(Id);
+
+			if (product == null)
+			{
+				return HttpNotFound();
+			}
+
+			return View(product);
 		}
 
 		public ActionResult About()
