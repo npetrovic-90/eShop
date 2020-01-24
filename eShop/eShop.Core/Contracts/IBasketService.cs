419a48347dde301ad737c25f6ef04635a0c5e503
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2020-01-24 16:23:03 +0100

    Added functionality to Basket: Create,Get Added functionality to BasketItems: Get,Remove,Add,Summary

---
+++ eShop/eShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,14 @@
+﻿using eShop.Core.ViewModels;
+using System.Collections.Generic;
+using System.Web;
+
+namespace eShop.Services
+{
+	public interface IBasketService
+	{
+		void AddToBasket(HttpContextBase httpContext, string productId);
+		List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+		BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+		void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+	}
+}
