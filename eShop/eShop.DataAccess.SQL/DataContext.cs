b682d7a3f4b594f177cc5bebf1dc4cf9be9682ea
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2020-01-24 12:02:03 +0100

    Added Basket Model and BasketItem model.

--- eShop/eShop.DataAccess.SQL/DataContext.cs
+++ eShop/eShop.DataAccess.SQL/DataContext.cs
@@ -17,5 +17,8 @@ namespace eShop.DataAccess.SQL
 
 		public DbSet<Product> Products { get; set; }
 		public DbSet<ProductCategory> ProductCategories { get; set; }
+		public DbSet<Basket> Baskets { get; set; }
+		public DbSet<BasketItem> BasketItems { get; set; }
+
 	}
 }
